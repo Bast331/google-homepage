1e28d0120396e19feeb67581b77284c52fc9cae8
Bast <neubeli@web.de>
2021-05-24 21:52:23 +0200

    Icon hinzugefügt, Searchbar, Navbar und andere Kleinigkeiten verschönert

---
+++ javascript/main.js
@@ -0,0 +1,14 @@
+/* Javascript bzw jquery: focus, click auf icon */
+$(document).ready(function() {
+    /* Focus - boxshadow, dass nicht nur der hover effekt stimmt */
+    $('#searchbar').focus(function(event) {
+        $('.searchbar_container').addClass('focus')
+    })
+    $('#searchbar').blur(function(event) {
+        $('.searchbar_container').removeClass('focus')
+    })
+    /* Klick aufs Icon soll ins input feld springen */
+    $('.searchbar-icon').click(function(event) {
+        $('#searchbar').focus();
+    })
+})
